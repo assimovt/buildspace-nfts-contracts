c7f545be99df5091bf7d44b70bfa77c8f6c1415f
Tair Asim <tair@hey.com>
2021-09-28 23:00:06 +0200

    Event emit and dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/i25UT5pGpbVjymVHNieaGgZNEs35IQ_f',
-      accounts: ['94f9a85d0f2c8e07e54f693377443b64616dbe747eb45e6ce50acc9c36d14c1a'],
+      url: process.env.DEV_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 }
